f6c1ab1b5f2a7684759ecb704473f60544b0dffd
StarFox <yundhun@gmail.com>
2022-10-08 12:37:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ For further information contact Marco Rudolph (rudolph@tnt.uni-hannover.de)'''
 import config as c
 from train import train
 from utils import load_datasets, make_dataloaders
+from sd import get_sd
 
-train_set, test_set = load_datasets(c.dataset_path, c.class_name)
+sd_dims = get_sd(c.dataset_path, c.class_name,False)
+
+train_set, test_set, _ = load_datasets(c.dataset_path, c.class_name)
 train_loader, test_loader = make_dataloaders(train_set, test_set)
-model = train(train_loader, test_loader)
+model = train(train_loader, test_loader, sd_dims)
